4dfcd8af90d681073a14569063a036f621bd431a
NotCoolMan <138533348+notcoolg@users.noreply.github.com>
2024-12-01 22:11:33 -0500

    1.0.7

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const config = {
 
 // Initialize the Auth0 client
 async function createAuth0Client() {
-  auth0Client = new Auth0Client({
-    domain: config.domain,
-    client_id: config.clientId,
-    redirect_uri: config.redirectUri
-  });
+    auth0Client = new Auth0Client({
+        domain: config.domain,
+        client_id: config.clientId,
+        redirect_uri: config.redirectUri
+      });      
 }
 
 // Get Management API Access Token
